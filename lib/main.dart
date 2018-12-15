6ba802511cea4e7cca6c23d2ef7007b7d2da5948
senamit2708 <32201389+senamit2708@users.noreply.github.com>
2018-12-15 11:34:03 +0530

    basic of flutter..entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+//basic of widget..how to run your first code in flutter
+//main is the starting point of the flutter
+//inside main method runApp method is called for the starting of the app.
+//first widget is Center
+//child widget of center is text
+//hint of closing of widget is always shown in comment line
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.rtl,
+      ),
+    ),
+  );
+}
