987eff0191f6f7186869861cbaf506f097eecca4
senamit2708 <32201389+senamit2708@users.noreply.github.com>
2018-12-19 21:33:17 +0530

    textfield

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 //basic of widget..how to run your first code in flutter
-//main is the starting point of the flutter
-//inside main method runApp method is called for the starting of the app.
-//first widget is Center
-//child widget of center is text
-//hint of closing of widget is always shown in comment line
+////main is the starting point of the flutter
+////inside main method runApp method is called for the starting of the app.
+////first widget is Center
+////child widget of center is text
+////hint of closing of widget is always shown in comment line
+////void main() {
+////  runApp(
+////    Center(
+////      child: Text(
+////        'Hello, world!',
+////        textDirection: TextDirection.rtl,
+////      ),
+////    ),
+////  );
+////}
+
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.rtl,
+  runApp(MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    home: MyScaffold(),
+  ));
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Container(
+
+          ),
+
+          Expanded(
+            child: Center(
+//              child: Text('Hello, world!'),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Enter your username'
+              ),
+            ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
